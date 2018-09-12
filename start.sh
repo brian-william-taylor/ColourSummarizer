bea3a259a35141d6e4634d5179cb504aec0e1512
Patrick <entscheidungsproblem@protonmail.com>
2018-09-12 17:53:21 -0400

    Added start script

--- start.sh
+++ start.sh
@@ -2,6 +2,12 @@
 
 # Make sure we are at git root
 cd `git rev-parse --show-toplevel`
+
+# Remove old binary if it exists
+if [ -f k-means ]; then
+	rm k-means
+fi
+
 # Download latest k-means binary
 wget $(curl -s https://api.github.com/repos/entscheidungsproblem/K-means/releases/latest | grep 'browser_' | cut -d\" -f4)
 
