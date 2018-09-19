3e3e0d9f5d7b5db7acdc0918e24e0872393afbf5
Patrick <entscheidungsproblem@protonmail.com>
2018-09-18 21:17:47 -0400

    Started work on using output from K-means

--- index.js
+++ index.js
@@ -18,8 +18,12 @@ var upload = multer({ storage: storage });
 const app = express();
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
-  cmd.get('./K-Means/K-Means -j ./uploads/' + req.file.filename,
+  cmd.get('./K-Means/k-means ./uploads/' + req.file.filename + 'print -j',
         function(err, data, stderr){
+            //obj = JSON.parse(data);
+            //console.log(obj.colors.color0); 
+            console.log(req.file.filename);           
+            console.log(data);
             var filePath = './uploads/' + req.file.filename;
             fs.access(filePath, error => {
                 if (!error) {
@@ -32,13 +36,24 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
             });
 
             res.json([
-            			"#0B0902",
-            			"#56667C",
-            			"#AA9869",
-            			"#3F2309",
-            		  "#8FB0C6",
-            			"#252E3C",
-            			"#0C0D12"
+              "#0B0902",
+              "#56667C",
+              "#AA9869",
+              "#3F2309",
+              "#8FB0C6",
+              "#252E3C",
+              "#0C0D12"
+              /*
+              obj.colors.color0,
+              obj.colors.color1,
+              obj.colors.color2,
+              obj.colors.color3,
+              obj.colors.color4,
+              obj.colors.color5,
+              obj.colors.color6
+              */
+
+
             	]);
          }
      );
